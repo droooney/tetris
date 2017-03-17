9815d1dde1f779f2785a6fa752d6e043a3ef440c
droooney <d.vor7@mail.ru>
2017-03-17 20:03:37 +0500

    Finish game basics.

--- figure.py
+++ figure.py
@@ -1,43 +1,144 @@
 from random import randint
+from copy import deepcopy
 from formatter import Formatter
+from timer import Timer
+from constants import *
 
-NEXT_FIGURE_RECT_SIZE = 15
+INFINITY = float('inf')
 
 
 class Figure(Formatter):
-    def __init__(self, *, name, cells, color, canvas, side_canvas):
+    def __init__(self, *, name, cells, center, color, app):
         self.name = name
-        self.cells = cells
+        self.cells = deepcopy(cells)
         self.color = color
-        self.canvas = canvas
-        self.side_canvas = side_canvas
-        self.center = self.cells[self.cells.index([0, 1])]
+        self.app = app
+        self.center = center
         self.rotation = gen_rand_rotation()
+        self.is_inserted = False
+        self.changing = False
+
+        self.calculate_min_and_max()
         self.rotate()
         self.put_in_side_canvas()
 
+    def rotate_right(self):
+        self.rotation = 1
+        self.rotate()
+
     def rotate(self):
-        pass
+        new_cells = []
+        new_center = self.transform(self.center, True)
+        offset_y = 0
+        offset_x = 0
+
+        for cell in self.cells:
+            new_cells.append(self.transform(cell))
+
+        if self.is_inserted:
+            field = self.app.field
+
+            for (y, x) in new_cells:
+                if y < 0 and offset_y < -y:
+                    offset_y = -y
+
+                if y >= HEIGHT and offset_y > HEIGHT - 1 - y:
+                    offset_y = HEIGHT - 1 - y
+
+                if x < 0 and offset_x < -x:
+                    offset_x = -x
+
+                if x >= WIDTH and offset_x > WIDTH - 1 - x:
+                    offset_x = WIDTH - 1 - x
+
+            for (ix, cell) in enumerate(new_cells):
+                (y, x) = new_cells[ix] = self.move_cell(cell, offset_y, offset_x)
+
+                if field[y][x] and not self.find_own_rect(field[y][x]):
+                    print('impossible')
+                    return
+
+        new_center = self.move_cell(new_center, offset_y, offset_x)
+
+        self.delete_cells()
+        self.replace_cells(new_cells, new_center)
+        self.calculate_min_and_max()
+
+    def move(self, offset_y, offset_x):
+        new_cells = []
+        new_center = self.move_cell(self.center, offset_y, offset_x)
+
+        for cell in self.cells:
+            new_cell = self.move_cell(cell, offset_y, offset_x)
+            new_cells.append(new_cell)
+
+        self.delete_cells()
+        self.replace_cells(new_cells, new_center)
+        self.calculate_min_and_max()
+        # print(self)
+
+    def move_cell(self, cell, offset_y, offset_x):
+        return cell[0] + offset_y, cell[1] + offset_x
+
+    def find_own_rect(self, Rect):
+        for (y, x, rect) in self.cells:
+            if rect == Rect:
+                return True
+
+    def delete_cells(self):
+        field = self.app.field
+
+        for (y, x, *rest) in self.cells:
+            if not len(rest):
+                continue
+
+            field[y][x] = None
+
+    def replace_cells(self, new_cells, new_center):
+        if self.changing or not self.is_inserted:
+            return
+
+        self.changing = True
+        field = self.app.field
+        field_canvas = self.app.field_canvas
+
+        for (i, cell) in enumerate(self.cells):
+            (y, x, *rest) = cell
+            (cell[0], cell[1]) = (new_y, new_x) = new_cells[i]
+
+            if not len(rest):
+                continue
+
+            rect = rest[0]
+            field[new_y][new_x] = rect
+            field_canvas.move(rect, (new_x - x) * SIZE, (new_y - y) * SIZE)
+
+        self.center = new_center
+        self.changing = False
+
+    def calculate_min_and_max(self):
+        self.y_min = min(*self.cells, key=lambda cell: cell[0])[0]
+        self.x_min = min(*self.cells, key=lambda cell: cell[1])[1]
+
+        self.y_max = max(*self.cells, key=lambda cell: cell[0])[0]
+        self.x_max = max(*self.cells, key=lambda cell: cell[1])[1]
+
+        self.y_len = self.y_max - self.y_min + 1
+        self.x_len = self.x_max - self.x_min + 1
 
     def put_in_side_canvas(self):
-        self.side_canvas.delete('all')
+        side_canvas = self.app.next_figure_canvas
 
-        y_min = min(*self.cells, key=lambda cell: cell[0])
-        y_max = max(*self.cells, key=lambda cell: cell[0])
-        y_len = y_max[0] - y_min[0] + 1
+        side_canvas.delete('all')
 
-        x_min = min(*self.cells, key=lambda cell: cell[1])
-        x_max = max(*self.cells, key=lambda cell: cell[1])
-        x_len = x_max[1] - x_min[1] + 1
-
-        y0 = NEXT_FIGURE_RECT_SIZE * ((4 - y_len) / 2) + 5
-        x0 = NEXT_FIGURE_RECT_SIZE * ((4 - x_len) / 2) + 5
+        y0 = NEXT_FIGURE_RECT_SIZE * ((4 - self.y_len) / 2 - self.y_min) + 5
+        x0 = NEXT_FIGURE_RECT_SIZE * ((4 - self.x_len) / 2 - self.x_min) + 5
 
         for cell in self.cells:
             y = y0 + cell[0] * NEXT_FIGURE_RECT_SIZE + 3
             x = x0 + cell[1] * NEXT_FIGURE_RECT_SIZE + 3
 
-            self.side_canvas.create_rectangle(
+            side_canvas.create_rectangle(
                 x,
                 y,
                 x + NEXT_FIGURE_RECT_SIZE,
@@ -47,9 +148,105 @@ class Figure(Formatter):
 
         pass
 
+    def transform(self, cell, is_center=False):
+        (center_y, center_x, *rest1) = self.center
+        rotation = self.rotation
+        (y, x, *rest2) = cell
+        res = (y, x) = (y - center_y, x - center_x)
+
+        if rotation == 1:
+            res = (x, -y)
+        elif rotation == 2:
+            res = (-y, -x)
+        elif rotation == 3:
+            res = (-x, y)
+
+        if is_center:
+            return center_y + res[0], center_x + res[1]
+
+        return int(center_y + res[0]), int(center_x + res[1])
+
+    def create_timer(self):
+        self.app.timer = Timer(self.app.interval, self.move_each_interval)
+
+    def move_to_the_side(self, offset_x):
+        field = self.app.field
+
+        for y in range(self.y_min, self.y_max + 1):
+            (func, val) = (max, -INFINITY) if offset_x > 0 else (min, INFINITY)
+            x = func(*self.cells, key=lambda cell: cell[1] if cell[0] == y else val)[1] + offset_x
+
+            if x >= WIDTH or x < 0 or field[y][x]:
+                break
+        else:
+            self.move(0, offset_x)
+
+    def move_down(self):
+        field = self.app.field
+
+        for x in range(self.x_min, self.x_max + 1):
+            y = max(*self.cells, key=lambda cell: cell[0] if cell[1] == x else -INFINITY)[0] + 1
+
+            if y >= HEIGHT or field[y][x]:
+                return
+
+        self.move(1, 0)
+
+        return True
+
+    def move_each_interval(self):
+        if self.move_down():
+            self.create_timer()
+        else:
+            self.stop()
+
+    def stop(self):
+        self.is_inserted = False
+        self.app.check_lines()
+
     def insert(self):
-        print(self)
-        pass
+        field = self.app.field
+        field_canvas = self.app.field_canvas
+
+        cells = self.cells
+        new_cells = []
+        top_point = TOP_LEFT_INSERT_POINT
+
+        if self.x_len < 3:
+            top_point += 1
+
+        for cell in self.cells:
+            y = cell[0] - self.y_min
+            x = top_point + cell[1] - self.x_min
+            canvas_cell = field[y][x]
+
+            if canvas_cell:
+                self.app.end_game()
+                return
+
+            new_cells.append([y, x])
+
+        for (ix, new_cell) in enumerate(new_cells):
+            (i, j) = new_cell
+            cell = cells[ix]
+            y = i * SIZE + 3
+            x = j * SIZE + 3
+
+            field[i][j] = field_canvas.create_rectangle(
+                x,
+                y,
+                x + SIZE,
+                y + SIZE,
+                fill=self.color
+            )
+
+            (cell[0], cell[1]) = (i, j)
+            cell.append(field[i][j])
+
+        self.center = (self.center[0] - self.y_min, top_point + self.center[1] - self.x_min)
+        self.calculate_min_and_max()
+        self.is_inserted = True
+        self.create_timer()
 
 
 FIGURES = {
@@ -60,11 +257,12 @@ FIGURES = {
     #
     'I': {
         'cells': [
+            [0, -1],
             [0, 0],
             [0, 1],
-            [0, 2],
-            [0, 3]
+            [0, 2]
         ],
+        'center': (0, 0),
         'color': 'cyan'
     },
 
@@ -75,11 +273,12 @@ FIGURES = {
     #
     'T': {
         'cells': [
+            [0, -1],
             [0, 0],
             [0, 1],
-            [0, 2],
-            [1, 1]
+            [1, 0]
         ],
+        'center': (0, 0),
         'color': 'purple'
     },
 
@@ -90,11 +289,12 @@ FIGURES = {
     #
     'L': {
         'cells': [
+            [0, -1],
             [0, 0],
             [0, 1],
-            [0, 2],
-            [1, 0]
+            [1, -1]
         ],
+        'center': (0, 0),
         'color': 'orange'
     },
 
@@ -105,11 +305,12 @@ FIGURES = {
     #
     'J': {
         'cells': [
+            [0, -1],
             [0, 0],
             [0, 1],
-            [0, 2],
-            [1, 2]
+            [1, 1]
         ],
+        'center': (0, 0),
         'color': 'blue'
     },
 
@@ -120,11 +321,12 @@ FIGURES = {
     #
     'O': {
         'cells': [
+            [0, -1],
             [0, 0],
-            [0, 1],
-            [1, 0],
-            [1, 1]
+            [1, -1],
+            [1, 0]
         ],
+        'center': (0.5, -0.5),
         'color': 'yellow'
     },
 
@@ -135,11 +337,12 @@ FIGURES = {
     #
     'S': {
         'cells': [
+            [0, 0],
             [0, 1],
-            [0, 2],
-            [1, 0],
-            [1, 1]
+            [1, -1],
+            [1, 0]
         ],
+        'center': (0, 0),
         'color': '#0f0'
     },
 
@@ -150,11 +353,12 @@ FIGURES = {
     #
     'Z': {
         'cells': [
+            [0, -1],
             [0, 0],
-            [0, 1],
-            [1, 1],
-            [1, 2]
+            [1, 0],
+            [1, 1]
         ],
+        'center': (0, 0),
         'color': 'red'
     }
 }
@@ -162,12 +366,12 @@ FIGURES = {
 FIGURES_NAMES = list(FIGURES.keys())
 
 for (f, value) in FIGURES.items():
-    FIGURES[f] = (lambda f, value: lambda *, canvas, side_canvas: Figure(
+    FIGURES[f] = (lambda f, value: lambda app: Figure(
         name=f,
         cells=value['cells'],
+        center = value['center'],
         color=value['color'],
-        canvas=canvas,
-        side_canvas=side_canvas
+        app=app
     ))(f, value)
 
 ROTATIONS = {
--- index.py
+++ index.py
@@ -1,31 +1,18 @@
 from tkinter import *
+from functools import reduce
 from timer import Timer
-from figure import gen_rand_figure_constructor, NEXT_FIGURE_RECT_SIZE
-
-SIZE = 30
-WIDTH = 10
-HEIGHT = 20
-BORDER = 1
-
-CANVAS_WIDTH = SIZE * WIDTH + 1
-CANVAS_HEIGHT = SIZE * HEIGHT + 1
-
-SIDE_PANEL_WIDTH = 200
-SIDE_PANEL_HEIGHT = CANVAS_HEIGHT
-
-NEXT_FIGURE_CANVAS_WIDTH = NEXT_FIGURE_RECT_SIZE * 4 + 11
-NEXT_FIGURE_CANVAS_HEIGHT = NEXT_FIGURE_RECT_SIZE * 4 + 11
-
-CONTAINER_WIDTH = CANVAS_WIDTH + 6 + SIDE_PANEL_WIDTH
-CONTAINER_HEIGHT = CANVAS_HEIGHT + 6
-
-LEVEL_1_INTERVAL = 1
-SPEED_INCREASE_COEFF = 0.9
+from figure import gen_rand_figure_constructor
+from constants import *
 
 
 class Tetris(Frame):
     def __init__(self, **kwargs):
         Frame.__init__(self, **kwargs)
+
+        self.status = None
+        self.timer = Timer(0, lambda: 0)
+        self.clear_timer = Timer(0, lambda: 0)
+        self.current_figure = None
 
         self.grid()
         self.grid_columnconfigure(0, minsize=CANVAS_WIDTH)
@@ -34,8 +21,7 @@ class Tetris(Frame):
 
         self.create_field()
         self.create_side_panel()
-
-        self.start_game()
+        self.change_status(GAME_ENDED)
 
     def create_field(self):
         canvas = self.field_canvas = Canvas(
@@ -60,14 +46,31 @@ class Tetris(Frame):
                 y = i * SIZE + 3
                 x = j * SIZE + 3
 
-                cell = canvas.create_rectangle(
+                canvas.create_rectangle(
                     x,
                     y,
                     x + SIZE,
                     y + SIZE
                 )
 
-                row.append(cell)
+                row.append(None)
+
+    def empty_field(self):
+        new_field = []
+
+        for i in range(HEIGHT):
+            row = []
+            new_field.append(row)
+
+            for j in range(WIDTH):
+                row.append(None)
+
+                cell = self.field[i][j]
+
+                if cell:
+                    self.field_canvas.delete(cell)
+
+        self.field = new_field
 
     def create_side_panel(self):
         frame = self.side_panel = Frame(
@@ -84,6 +87,7 @@ class Tetris(Frame):
 
         self.create_points_frame()
         self.create_next_figure_panel()
+        self.create_buttons()
 
     def create_points_frame(self):
         points_frame = Frame(
@@ -131,7 +135,7 @@ class Tetris(Frame):
             next_figure_frame,
             text='Next:'
         ).grid(
-            row=1,
+            row=0,
             column=0
         )
 
@@ -143,36 +147,221 @@ class Tetris(Frame):
             bg='#eee'
         )
         self.next_figure_canvas.grid(
-            row=4,
+            row=1,
             column=0
         )
 
+    def create_buttons(self):
+        buttons = Frame(self.side_panel)
+        buttons.grid(
+            row=2,
+            column=0,
+            pady=10
+        )
+
+        self.start_game_button = Button(
+            buttons,
+            text='Start',
+            command=self.start_game
+        )
+        self.start_game_button.grid(
+            row=0,
+            column=0
+        )
+
+        self.pause_game_caption = StringVar(value='Pause')
+
+        self.pause_game_button = Button(
+            buttons,
+            state=DISABLED,
+            textvariable=self.pause_game_caption,
+            command=self.pause_or_continue_game
+        )
+        self.pause_game_button.grid(
+            row=1,
+            column=0
+        )
+
+        self.end_game_button = Button(
+            buttons,
+            state=DISABLED,
+            text='End',
+            command=self.end_game
+        )
+        self.end_game_button.grid(
+            row=2,
+            column=0
+        )
+
+    def on_key_pressed(self, event):
+        sym = event.keysym
+
+        if sym == 'a' or sym == 'Left':
+            # move left
+
+            if self.current_figure and self.current_figure.is_inserted:
+                self.current_figure.move_to_the_side(-1)
+        elif sym == 'd' or sym == 'Right':
+            # move right
+
+            if self.current_figure and self.current_figure.is_inserted:
+                self.current_figure.move_to_the_side(1)
+        elif sym == 's' or sym == 'Down':
+            # speed up falling
+
+            if self.current_figure and self.current_figure.is_inserted:
+                self.current_figure.move_down()
+        elif sym == 'f':
+            # fall immediately
+            print('Fall')
+        elif sym == 'space':
+            # rotate figure
+
+            if self.current_figure and self.current_figure.is_inserted:
+                self.current_figure.rotate_right()
+
     def on_quit(self):
         print('quitting')
+        self.end_game()
         root.destroy()
-        # self.timer.cancel()
+
+    def change_status(self, status):
+        start_game_button_state = DISABLED
+        pause_game_button_state = DISABLED
+        end_game_button_state = DISABLED
+        pause_button_caption = 'Pause'
+
+        if status == IN_GAME:
+            pause_game_button_state = NORMAL
+            end_game_button_state = NORMAL
+        elif status == GAME_ENDED:
+            start_game_button_state = NORMAL
+        elif status == GAME_PAUSED:
+            pause_game_button_state = NORMAL
+            end_game_button_state = NORMAL
+            pause_button_caption = 'Continue'
+
+        self.status = status
+        self.start_game_button['state'] = start_game_button_state
+        self.pause_game_button['state'] = pause_game_button_state
+        self.end_game_button['state'] = end_game_button_state
+        self.pause_game_caption.set(pause_button_caption)
 
     def start_game(self):
+        self.set_level(1)
+        self.set_score_coeff(1)
+        self.set_lines_cleared(0)
+        self.change_status(IN_GAME)
         self.set_interval(LEVEL_1_INTERVAL)
         self.set_score(0)
+        self.empty_field()
         self.generate_figure()
         self.insert_figure()
 
+    def pause_or_continue_game(self):
+        if self.status == IN_GAME:
+            self.pause_game()
+        elif self.status == GAME_PAUSED:
+            self.continue_game()
+
+    def pause_game(self):
+        self.change_status(GAME_PAUSED)
+        self.timer.pause()
+        self.clear_timer.pause()
+
+    def continue_game(self):
+        self.change_status(IN_GAME)
+        self.timer.resume()
+        self.clear_timer.resume()
+
+    def end_game(self):
+        self.change_status(GAME_ENDED)
+
+        self.timer.cancel()
+        self.clear_timer.cancel()
+
+        print('end game')
+
+    def set_lines_cleared(self, lines):
+        self.lines_cleared = lines
+
+    def set_level(self, level):
+        self.level = level
+
     def set_interval(self, interval):
         self.interval = interval
+
+    def set_score_coeff(self, coeff):
+        self.score_coeff = coeff
 
     def set_score(self, score):
         self.score = score
         self.points.set(score)
 
     def generate_figure(self):
-        self.next_figure = gen_rand_figure_constructor()(
-            side_canvas=self.next_figure_canvas,
-            canvas=self.field_canvas
-        )
+        self.next_figure = gen_rand_figure_constructor()(self)
 
     def insert_figure(self):
-        self.next_figure.insert()
+        self.current_figure = self.next_figure
+        self.current_figure.insert()
+        self.generate_figure()
+
+    def check_lines(self):
+        field = self.field
+        lines = []
+
+        for (y, row) in enumerate(self.field):
+            if not all(row):
+                continue
+
+            lines.append(y)
+
+            for (x, rect) in enumerate(row):
+                field[y][x] = None
+                self.field_canvas.delete(rect)
+
+        if not lines:
+            self.insert_figure()
+
+            return
+
+        score = POINTS[len(lines)]
+
+        self.set_lines_cleared(self.lines_cleared + len(lines))
+
+        level = self.lines_cleared // 10
+
+        if self.level != level:
+            self.set_level(level)
+            self.set_interval(LEVEL_1_INTERVAL * (SPEED_INCREASE_COEFF ** level))
+            self.set_score_coeff(SCORE_INCREASE_COEFF ** level)
+
+        self.set_score(self.score + int(score*self.score_coeff))
+
+        self.clear_timer = Timer(0.5, lambda: self.fall(lines))
+
+    def fall(self, lines):
+        field = self.field
+
+        for (y, row) in enumerate(reversed(self.field)):
+            y = HEIGHT - 1 - y
+
+            if y in lines:
+                continue
+
+            rows_to_fall = reduce((lambda s, i: s + (1 if i > y else 0)), lines, 0)
+
+            if any(field[y + rows_to_fall]) and rows_to_fall:
+                print('Error:', field[y + rows_to_fall], y, y + rows_to_fall)
+
+            for (x, rect) in enumerate(row):
+                field[y][x] = None
+                field[y + rows_to_fall][x] = rect
+
+                if rect:
+                    self.field_canvas.move(rect, 0, rows_to_fall * SIZE)
+
+        self.insert_figure()
 
 
 root = Tk()
@@ -183,5 +372,6 @@ my_app.master.minsize(CONTAINER_WIDTH, CONTAINER_HEIGHT)
 my_app.master.maxsize(CONTAINER_WIDTH, CONTAINER_HEIGHT)
 
 root.protocol('WM_DELETE_WINDOW', my_app.on_quit)
+root.bind('<Key>', my_app.on_key_pressed)
 
 root.mainloop()
